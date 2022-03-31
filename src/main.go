ec422db41dbc510deaffa38169f8358fd819e330
termy <termy>
2022-03-31 06:47:39 +0000

    starting filewatch dev

---
+++ src/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+)
+func localWatchHandler(watcher *fsnotify.Watcher){
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+}
+func filewatch(file string)(*fsnotify.Watcher){
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+//	done := make(chan bool)
+	
+
+	err = watcher.Add(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//<-done
+	return watcher
+}
+func main() {
+  fmt.Println("Startup!")
+// Start file watch
+watcher := filewatch("/tmp/foo")
+go localWatchHandler(watcher)
+select{}
+}
